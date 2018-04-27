ceb5a5a0ad30eac85ff3af3835871d44a1de3ba4
barklow <blacklucifieroff@gmail.com>
2018-04-28 00:01:52 +0300

    php+ajax

--- summary/dub.php
+++ summary/dub.php
@@ -1,5 +1,11 @@
-<?php 
-
-echo "string";
-
- ?>
+<?php
+echo 'Часов, минут и секунд: ' . date('h:i:s');
+echo '<br>';
+echo 'Часов и минут: ' . date('h:i');
+echo '<br>';
+echo 'Часов: ' . date('h');
+echo '<br>';
+echo 'Минут: ' . date('i');
+echo '<br>';
+echo 'Секунд: '. date('s');
+?>
